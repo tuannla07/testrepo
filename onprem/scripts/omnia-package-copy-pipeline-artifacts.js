d47d17ef8f87937b2ff859a473cc32d845b98dbc
Tuan Nguyen Le <tuan.nguyen.le@omniaintranet.com>
2025-10-23 10:30:50 +0700

    test

--- onprem/scripts/omnia-package-copy-pipeline-artifacts.js
+++ onprem/scripts/omnia-package-copy-pipeline-artifacts.js
@@ -1,6 +1,14 @@
 const fs = require("fs-extra");
 const path = require("path");
 
+const incomingArgs = process.argv.slice(2);
+if (incomingArgs.length !== 2) {
+  console.error('Please provide at least Omnia CLI directory and SRC directory.');
+  process.exit(1);
+}
+
+const [srcFolder, destDir] = incomingArgs;
+
 const currentPath = process.cwd();
 
 const folders = fs.readdirSync(currentPath)
@@ -11,9 +19,6 @@ console.log(folders);
 
 /////////////////////////////////////////////////////////////////////////////////////
 
-
-const srcFolder = $(Build.SourcesDirectory);
-const destDir = $(System.ArtifactsDirectory);
 const destPackageFolders = destDir + "/packages";
 
 if (!fs.existsSync(destPackageFolders)) {
