3202e0771f47d128d9fb0a6c6e0c9f68e7071846
Tuan Nguyen Le <tuan.nguyen.le@omniaintranet.com>
2025-10-24 15:09:40 +0700

    3

--- onprem/scripts/omnia-package-copy-pipeline-artifacts.js
+++ onprem/scripts/omnia-package-copy-pipeline-artifacts.js
@@ -2,35 +2,21 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const incomingArgs = process.argv.slice(2);
-if (incomingArgs.length !== 2) {
-  console.error('Please provide at least Omnia CLI directory and SRC directory.');
+if (incomingArgs.length < 3) {
+  console.error('Please provide: artifacts directory, src directory and comma-separated package names.');
   process.exit(1);
 }
 
-const [srcFolder, destDir] = incomingArgs;
+const srcFolder = "" //$(Build.SourcesDirectory);
+const destDir = "C:\\tmp"; //$(System.ArtifactsDirectory);
+const destPackageFolders = destinationFolder + "/packages";
 
-const currentPath = process.cwd();
-
-const folders = fs.readdirSync(currentPath)
-  .filter(name => fs.statSync(path.join(currentPath, name)).isDirectory());
-
-console.log('Folders in current path:');
-console.log(folders);
-
-/////////////////////////////////////////////////////////////////////////////////////
-
-const destPackageFolders = destDir + "/packages";
-
-if (!fs.existsSync(destPackageFolders)) {
-    fs.mkdirSync(destPackageFolders, { recursive: true});
+if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(packageFolders, { recursive: true});
 }
 
-console.log('Current working dir:', process.cwd());
-console.log('Script dir:', __dirname);
-console.log('Resolved template path:', path.resolve(__dirname, '../package-templates'));
-
 // copy package template to destination packages folder
-const templateFolder = path.resolve(__dirname, '../package-templates');
+const templateFolder = "../package-templates";
 
 fs.copySync(templateFolder, destDir, {overwrite: true, recursive: true});
-console.log(`Copied artifacts from ${srcFolder} to ${destPackageFolders}`);
+console.log(`Copied artifacts from ${srcFolder} to ${packageFolders}`);
